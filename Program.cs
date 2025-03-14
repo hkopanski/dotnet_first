08a53c485d7998d4ba8e33e30bd6fed9fc91c1cf
hkuser <hkuser@hkuser-B365M-DS3H.attlocal.net>
2025-03-13 22:11:23 -0400

    program updates

--- Program.cs
+++ Program.cs
@@ -17,6 +17,24 @@ namespace MyFirstProject
             int max = int.MaxValue;
             int min = int.MinValue;
             Console.WriteLine($"The range of integers is {min} to {max}");
-        }
+            // The Three Parts of a LINQ Query:
+            // 1. Data source.
+            int[] numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+            var evenNumQuery = from num in numbers
+                   where (num % 2) == 0
+                   select num;
+
+            int evenNumCount = evenNumQuery.Count();
+            foreach (int num in evenNumQuery)
+{
+    Console.WriteLine("{0,1} ", num);
+}
+            Console.WriteLine($"There are {evenNumCount} even numbers");
+
     }
+}
+
 }
